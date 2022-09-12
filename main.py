4d058ce03e2ec4a0c7e1edaaacef63165b3df9c8
YaGitHub <eufo312@gmail.com>
2022-09-13 00:34:37 +0900

    1st_commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+def main():
+    st.title('ピンボケ除去')
+    st.write('〇画像アップロード')
+    uploaded_file = st.file_uploader("画像をアップロードしてください。")
+    image=Image.open(uploaded_file)
+    img_array = np.array(image)
+    st.write('〇元画像↓')
+    st.image(img_array,caption = 'もと',use_column_width = True)
+    st.write('〇変換後画像↓')
+    trans_img_array = trans(img_array)
+    st.image(trans_img_array,caption = 'へんかん',use_column_width = True)
+
+def trans(img):
+    return img-50
+
+if __name__=="__main__":
+    main()
